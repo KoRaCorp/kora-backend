49923a7a8e3660a923f1616eeca7aa058c1dfd37
jongwanra <najongsi@gmail.com>
2024-03-30 12:00:47 +0900

    [#1] feat: project setting with spring initializer

---
+++ src/test/java/corp/koraserver/KoraServerApplicationTests.java
@@ -0,0 +1,13 @@
+package corp.koraserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoraServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
