f33b7de265cf48b850f5b34f4ebd654115deb869
jongwanra <najongsi@gmail.com>
2024-03-30 14:31:56 +0900

    [#2] chore: scraping client 추가 및 데이터 스크랩핑 가능 여부 확인 완료

---
+++ kora-client/src/main/java/corp/kora/scrap/client/ScrapByUrlClient.java
@@ -0,0 +1,20 @@
+package corp.kora.scrap.client;
+
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScrapByUrlClient {
+
+	public void scrapByUrl(String url) {
+		try {
+			Document document = Jsoup.connect(url).get();
+			System.out.println("document = " + document);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
